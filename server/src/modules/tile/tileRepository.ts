8d40197ad95032492e7e9fb013ddb14b21a4ed1c
Lise PERARD <liseprd@hotmail.fr>
2025-01-13 12:31:02 +0100

    début de middleware 5/7 tests passés

--- server/src/modules/tile/tileRepository.ts
+++ server/src/modules/tile/tileRepository.ts
@@ -22,7 +22,11 @@ class TileRepository {
   }
 
   async readByCoordinates(coordX: number, coordY: number) {
-    // your code here
+    const [rows] = await databaseClient.query<Rows>(
+      "select coord_x, coord_y from tile where coord_x = ? AND coord_y=?",
+      [coordX, coordY],
+    );
+    return rows as Tile[];
   }
 
   async getRandomIsland() {
--- server/src/router.ts
+++ server/src/router.ts
@@ -21,7 +21,7 @@ router.post("/api/games", gameActions.add);
 
 import tileActions from "./modules/tile/tileActions";
 
-router.get("/api/tiles", tileActions.browse);
+router.get("/api/tiles", tileActions.validate, tileActions.browse);
 
 /* ************************************************************************* */
 
