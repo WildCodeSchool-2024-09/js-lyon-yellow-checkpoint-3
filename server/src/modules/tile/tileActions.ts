a09f1a55c7897cf53b2fd5576c5836cb2cd0a6ef
maldonae <maldonadoeric69120@gmail.com>
2025-01-13 13:18:23 +0100

    step4

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -16,10 +16,19 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const { coord_x, coord_y } = req.body;
+    // Vérifie que coord_x et coord_y sont bien des entiers et dans les limites autorisées
+    if (!Number.isInteger(coord_x) || !Number.isInteger(coord_y)) {
+      res.sendStatus(422); // Envoie une réponse d'erreur
+    } else if (coord_x < 0 || coord_x > 11 || coord_y < 0 || coord_y > 5) {
+      res.sendStatus(422); // Envoie une réponse d'erreur
+    } else {
+      next(); // Les coordonnées sont valides, passe au middleware suivant
+    }
+  } catch (err) {
+    next(err); // Si une erreur se produit, passe à l'erreur suivante
+  }
 };
 
-export default {
-  browse,
-  validate,
-};
+export default { browse, validate };
--- server/src/router.ts
+++ server/src/router.ts
@@ -7,18 +7,16 @@ const router = express.Router();
 /* ************************************************************************* */
 
 import boatActions from "./modules/boat/boatActions";
-
-router.get("/api/boats", boatActions.browse);
-router.put("/api/boats/:id", boatActions.edit);
-
 import tileActions from "./modules/tile/tileActions";
 
+router.get("/api/boats", boatActions.browse);
+router.put("/api/boats/:id", tileActions.validate, boatActions.edit);
 router.get("/api/tiles", tileActions.browse);
+router.post("/api/tiles", tileActions.validate);
 
 import gameActions from "./modules/game/gameActions";
 
 router.post("/api/games", gameActions.add);
-
 /* ************************************************************************* */
 
 export default router;
