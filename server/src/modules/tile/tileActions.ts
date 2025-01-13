deb534ebc6aa38050e55db3db9f661947326dc86
Angelica Lazaro <angelicamld@gmail.com>
2025-01-13 12:53:07 +0100

    step4 passé, c'est du nazisme c truc

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -5,10 +5,10 @@ import tileRepository from "./tileRepository";
 const browse: RequestHandler = async (req, res, next) => {
   try {
     // Fetch all boats from the database
-    const boats = await tileRepository.readAll();
+    const tiles = await tileRepository.readAll();
 
     // Respond with the boats in JSON format
-    res.json(boats);
+    res.json(tiles);
   } catch (err) {
     // Pass any errors to the error-handling middleware
     next(err);
@@ -16,7 +16,20 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const { coord_x, coord_y } = req.body;
+
+    // Vérifie que coord_x et coord_y sont bien des entiers et dans les limites autorisées
+    if (!Number.isInteger(coord_x) || !Number.isInteger(coord_y)) {
+      res.sendStatus(422); // Envoie une réponse d'erreur
+    } else if (coord_x < 0 || coord_x > 11 || coord_y < 0 || coord_y > 5) {
+      res.sendStatus(422); // Envoie une réponse d'erreur
+    } else {
+      next(); // Les coordonnées sont valides, passe au middleware suivant
+    }
+  } catch (err) {
+    next(err); // Si une erreur se produit, passe à l'erreur suivante
+  }
 };
 
 export default {
