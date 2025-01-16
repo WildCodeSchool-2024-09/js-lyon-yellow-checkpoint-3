9806a9cb66cd3dd059aff7f79d8b191b9e60b5d6
Jules-wild <julesmarcilloux11@gmail.com>
2025-01-16 11:45:12 +0100

    Exercise 1 et 2 fait

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,9 +1,14 @@
 import type { RequestHandler } from "express";
+import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const rows = await tileRepository.readAll();
+    res.json(rows);
+  } catch (err) {
+    next(err);
+  }
 };
-
 const validate: RequestHandler = async (req, res, next) => {
   // your code here
 };
